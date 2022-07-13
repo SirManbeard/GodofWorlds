9f8c53b0772e0fc40eb1f7524f43deea2d9e1934
SirManbeard <redsalt824@gmail.com>
2022-07-13 10:03:59 -0500

    Added Guardian Shield (SMB_GuardianShield)

--- Plugin.cs
+++ Plugin.cs
@@ -21,8 +21,8 @@ namespace GodOfWorlds
         {
             Item leviathanAxe1H = new Item("leviathanwep", "SMB_LeviathanAxe1H");
             leviathanAxe1H.Crafting.Add(CraftingTable.Forge, 3);
-            leviathanAxe1H.Name.English("Leviathan Axe One Hand"); // You can use this to fix the display name in code
-            leviathanAxe1H.Description.English("An axe made the stop the gods.");
+            leviathanAxe1H.Name.English("Leviathan Axe"); // You can use this to fix the display name in code
+            leviathanAxe1H.Description.English("An Axe fit for a god, to end a god.");
             leviathanAxe1H.RequiredItems.Add("Iron", 15);
             leviathanAxe1H.RequiredItems.Add("Copper", 8);
             leviathanAxe1H.RequiredItems.Add("Silver", 3);
@@ -33,8 +33,8 @@ namespace GodOfWorlds
             
             Item leviathanAxe2H = new Item("leviathanwep", "SMB_LeviathanAxe2H");
             leviathanAxe2H.Crafting.Add(CraftingTable.Forge, 3);
-            leviathanAxe2H.Name.English("Leviathan Axe Two Hand"); // You can use this to fix the display name in code
-            leviathanAxe2H.Description.English("An axe made the stop the gods. But much bigger");
+            leviathanAxe2H.Name.English("Leviathan Axe Heavy"); // You can use this to fix the display name in code
+            leviathanAxe2H.Description.English("An axe made the stop the gods. But much bigger.");
             leviathanAxe2H.RequiredItems.Add("Iron", 15);
             leviathanAxe2H.RequiredItems.Add("Copper", 8);
             leviathanAxe2H.RequiredItems.Add("Silver", 3);
@@ -46,7 +46,7 @@ namespace GodOfWorlds
             Item leviathanAxe1Htest = new Item("leviathanwep", "SMB_LeviathanAxe1H_TEST");
             leviathanAxe1Htest.Crafting.Add(CraftingTable.Forge, 3);
             leviathanAxe1Htest.Name.English("Leviathan Axe One Hand TEST"); // You can use this to fix the display name in code
-            leviathanAxe1Htest.Description.English("An axe made the stop the gods.TEST");
+            leviathanAxe1Htest.Description.English("An Axe fit for a god, to end a god.TEST");
             leviathanAxe1Htest.RequiredItems.Add("Iron", 15);
             leviathanAxe1Htest.RequiredItems.Add("Copper", 8);
             leviathanAxe1Htest.RequiredItems.Add("Silver", 3);
@@ -54,6 +54,18 @@ namespace GodOfWorlds
             leviathanAxe1Htest.RequiredUpgradeItems.Add("Copper", 1);
             leviathanAxe1Htest.RequiredUpgradeItems.Add("Silver", 1);
             leviathanAxe1Htest.CraftAmount = 1;
+            
+            Item guardianShield = new Item("leviathanwep", "SMB_GuardianShield");
+            guardianShield.Crafting.Add(CraftingTable.Forge, 3);
+            guardianShield.Name.English("Guardian Shield"); // You can use this to fix the display name in code
+            guardianShield.Description.English("A shield made to stop the gods.");
+            guardianShield.RequiredItems.Add("Iron", 15);
+            guardianShield.RequiredItems.Add("Copper", 8);
+            guardianShield.RequiredItems.Add("Silver", 3);
+            guardianShield.RequiredUpgradeItems.Add("Iron", 7);
+            guardianShield.RequiredUpgradeItems.Add("Copper", 1);
+            guardianShield.RequiredUpgradeItems.Add("Silver", 1);
+            guardianShield.CraftAmount = 1;
         }
     }
 }
